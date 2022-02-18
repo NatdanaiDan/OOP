737bd2b270696dbe0ca0eeeea3a0e4f8f3471f97
unknown <firstfay92@gmail.com>
2022-02-18 15:35:46 +0700

    second

--- week4/lab4.py
+++ week4/lab4.py
@@ -1,12 +1,16 @@
+list_yes = ["yes", "y", "ye", "ok", "have"]
+list_no = ["no", "n", "nah", "nope", "not", "never"]
+
+
 class Agent:
     def __init__(self):
         self.property_list = []
 
     def add_property(self):
         type_map = {("house", "rental"): HouseRental,
-                    ("house", "purcahse"): HousePurchase,
+                    ("house", "purchase"): HousePurchase,
                     ("apartment", "rental"): ApartmentRental,
-                    ("apartment", "purcahse"): ApartmentPurchase}
+                    ("apartment", "purchase"): ApartmentPurchase}
         property_type = input("Enter property type: ")
         property_status = input("Enter property status: ")
         type_selection = type_map[(property_type, property_status)]
@@ -20,14 +24,33 @@ class Agent:
                 print(property)
         else:
             id = int(input("Enter property ID: "))
-            self.property_list[id].display()
+            for property in self.property_list:
+                if property.ID == id:
+                    print(property.display())
+
+    @staticmethod
+    def yn_convertor(choice):
+        if choice.lower() in list_yes:
+            return True
+        elif choice.lower() in list_no:
+            return False
+        else:
+            raise ValueError("Please enter yes or no")
 
 
 class Property:
+    property_id = 1
+
     def __init__(self, square_Metres, num_bedrooms, num_bathrooms, **kwargs):
         self.square_Metres = square_Metres
         self.num_bedrooms = num_bedrooms
         self.num_bathrooms = num_bathrooms
+        self._property_id = Property.property_id
+        Property.property_id += 1
+
+    @property
+    def ID(self):
+        return self._property_id
 
     def display(self):
         print("square_Metres:", self.square_Metres)
@@ -35,9 +58,15 @@ class Property:
         print("num_bathrooms:", self.num_bathrooms)
 
     def prompt_init():
-        return dict(square_Metres=input("Enter the square metres: "),
-                    num_bedrooms=input("Enter the number of bedrooms: "),
-                    num_bathrooms=input("Enter the number of bathrooms: "))
+        while True:
+            try:
+                return dict(square_Metres=float(input("Enter the square metres: ")),
+                            num_bedrooms=int(input(
+                                "Enter the number of bedrooms: ")),
+                            num_bathrooms=int(input("Enter the number of bathrooms: ")))
+
+            except ValueError:
+                print("Invalid input, try again")
 
 
 class House(Property):
@@ -54,9 +83,14 @@ class House(Property):
     @staticmethod
     def prompt_init():
         parent_init = Property.prompt_init()
-        parent_init.update(garages=input("Enter the number of garages: "),
-                           ferced_yard=input("Enter the number of ferced yard: "))
-        return parent_init
+        while True:
+            try:
+                parent_init.update(garages=int(input("Enter the number of garages: ")),
+                                   ferced_yard=Agent.yn_convertor(
+                    input("Enter the number of ferced yard(Y/n): ")))
+                return parent_init
+            except ValueError:
+                print("Invalid input, try again")
 
 
 class Apartment(Property):
@@ -70,12 +104,16 @@ class Apartment(Property):
         print(f"balcony: {self.balcony}")
         print(f"laundry_room: {self.laundry_room}")
 
-    @staticmethod
+    @ staticmethod
     def prompt_init():
         parent_init = Property.prompt_init()
-        parent_init.update(balcony=input("Have a balcony?: "),
-                           laundry_room=input("Have a laundry room?: "))
-        return parent_init
+        while True:
+            try:
+                parent_init.update(balcony=Agent.yn_convertor(input("Have a balcony?(Y/n): ")),
+                                   laundry_room=Agent.yn_convertor(input("Have a laundry room?(Y/n): ")))
+                return parent_init
+            except ValueError:
+                print("Invalid input, try again")
 
 
 class Rental:
@@ -83,10 +121,14 @@ class Rental:
         self.furnished = furnished
         self.rent = rent
 
-    @staticmethod
+    @ staticmethod
     def prompt_init():
-        return dict(furnished=input("Have a furniture?: "),
-                    rent=input("Enter the rental Price: "))
+        while True:
+            try:
+                return dict(furnished=Agent.yn_convertor(input("Have a furniture?(Y/n): ")),
+                            rent=int(input("Enter the rental Price: ")))
+            except ValueError:
+                print("Invalid input, try again")
 
     def display(self):
         print(f"furnished: {self.furnished}")
@@ -98,10 +140,14 @@ class Purchase:
         self.price = price
         self.taxes = taxes
 
-    @staticmethod
+    @ staticmethod
     def prompt_init():
-        return dict(price=input("Enter the price: "),
-                    taxes=input("Enter the taxes: "))
+        while True:
+            try:
+                return dict(price=int(input("Enter the price: ")),
+                            taxes=int(input("Enter the taxes: ")))
+            except ValueError:
+                print("Invalid input, try again")
 
     def display(self):
         print(f"price: {self.price}")
@@ -113,7 +159,7 @@ class HouseRental(House, Rental):
         House.__init__(self, **kwargs)
         Rental.__init__(self, **kwargs)
 
-    @staticmethod
+    @ staticmethod
     def prompt_init():
         return (House.prompt_init() | Rental.prompt_init())
 
@@ -128,7 +174,7 @@ class HousePurchase(House, Purchase):
         House.__init__(self, **kwargs)
         Purchase.__init__(self, **kwargs)
 
-    @staticmethod
+    @ staticmethod
     def prompt_init():
         return (House.prompt_init()) | (Purchase.prompt_init())
 
@@ -142,7 +188,7 @@ class ApartmentRental(Apartment, Rental):
         Apartment.__init__(self, **kwargs)
         Rental.__init__(self, **kwargs)
 
-    @staticmethod
+    @ staticmethod
     def prompt_init():
         return (Apartment.prompt_init()) | (Rental.prompt_init())
 
@@ -156,7 +202,7 @@ class ApartmentPurchase(Apartment, Purchase):
         Apartment.__init__(self, **kwargs)
         Purchase.__init__(self, **kwargs)
 
-    @staticmethod
+    @ staticmethod
     def prompt_init():
         return (Apartment.prompt_init()) | (Purchase.prompt_init())
 
